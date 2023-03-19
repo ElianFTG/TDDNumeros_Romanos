ade6a7c68c92307f6280d258ee4098a16bee2b7f
Elian <eliantito234@gmail.com>
2023-03-19 15:35:13 -0400

    Devuelve el 5 a V

--- src/romanos.js
+++ src/romanos.js
@@ -1,9 +1,20 @@
 function romano(num) {
     let cadena = "";
     if(num < 5){
-        for(var i = 1; i <= num; i += 1){
-            cadena += "I";
+        if(num == 4){
+            cadena += "IV";
         }
+        else {
+            for(var i = 1; i <= num; i += 1){
+                cadena += "I";
+            }
+        }
+    }
+    if (num == 5){
+        cadena = "V";
+    }
+    if (num == 9){
+        cadena = "IX"; 
     }
     return cadena;
 }
--- src/romanos.test.js
+++ src/romanos.test.js
@@ -4,8 +4,11 @@ describe("Romano", () => {
   it("Deberia retornar I", () => {
     expect(romano(1)).toEqual('I');
   });
-  it("Deberia retornar de 2 a II", () => {
-    expect(romano(2)).toEqual('II');
+  it("Deberia retornar de 4 a IV", () => {
+    expect(romano(4)).toEqual('IV');
+  });
+  it("Deberia retornar de 5 a V", () => {
+    expect(romano(5)).toEqual('V');
   });
 });
 
