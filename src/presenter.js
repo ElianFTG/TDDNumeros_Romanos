9e6d5ead71ad40fb04e75559c516289f397a5b0a
Elian <eliantito234@gmail.com>
2023-03-20 17:17:29 -0600

    Implementacion en html

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,13 @@
-import sumar from "./sumador";
+import romano from "./romanos";
 
 const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
+const form = document.querySelector("#romanos-form");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  div.innerHTML = "<p>" + romano(firstNumber) + "</p>";
 });
